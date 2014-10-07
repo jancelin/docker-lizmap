c11721b554db67a5cf19eb41d0628275b4d7d399
ANCELIN Julien <julien.ancelin@stlaurent.lusignan.inra.fr>
2014-10-07 15:21:41 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-unzip /web/release_2_10.zip -d /web
+unzip /web/master.zip -d /web
 
-rm /web/release_2_10.zip
+rm /web/master.zip
 
-chown -R :www-data /web/lizmap-web-client-release_2_10/temp/ \
-    /web/lizmap-web-client-release_2_10/lizmap/var/ \
-    /web/lizmap-web-client-release_2_10/lizmap/www \
-    /web/lizmap-web-client-release_2_10/lizmap/install/qgis/edition/ 
+chown -R :www-data /web/lizmap-web-client-master/temp/ \
+    /web/lizmap-web-client-master/lizmap/var/ \
+    /web/lizmap-web-client-master/lizmap/www \
+    /web/lizmap-web-client-master/lizmap/install/qgis/edition/ 
 
-chmod -R 775 /web/lizmap-web-client-release_2_10/temp/ \
-    /web/lizmap-web-client-release_2_10/lizmap/var/ \
-    /web/lizmap-web-client-release_2_10/lizmap/www \
-    /web/lizmap-web-client-release_2_10/lizmap/install/qgis/edition/
+chmod -R 775 /web/lizmap-web-client-master/temp/ \
+    /web/lizmap-web-client-master/lizmap/var/ \
+    /web/lizmap-web-client-master/lizmap/www \
+    /web/lizmap-web-client-master/lizmap/install/qgis/edition/
 
-rm -rf /web/lizmap-web-client-release_2_10/temp/lizmap/*
+rm -rf /web/lizmap-web-client-master/temp/lizmap/*
 
 #dupliquer lizmap en plusieurs sites
-cp -a /web/lizmap-web-client-release_2_10 /web/websig
+cp -a /web/lizmap-web-client-master /web/websig
 rm /web/websig/lizmap/var/jauth.db /web/websig/lizmap/var/logs.db /web/websig/lizmap/var/config/lizmapConfig.ini.php
 mkdir /home2  
 touch /home2/jauth.db /home2/logs.db /home2/lizmapConfig.ini.php

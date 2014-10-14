0d752217c9b4bb951227d3e473e096b11cc89692
ANCELIN Julien <julien.ancelin@stlaurent.lusignan.inra.fr>
2014-10-14 11:45:59 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-unzip /var/www/master.zip -d /var/www
+unzip /var/www/2.10.0.zip -d /var/www
 
-rm /var/www/master.zip
+rm /var/www/2.10.0.zip
 
-chown -R :www-data /var/www/lizmap-web-client-master/temp/ \
-    /var/www/lizmap-web-client-master/lizmap/var/ \
-    /var/www/lizmap-web-client-master/lizmap/www \
-    /var/www/lizmap-web-client-master/lizmap/install/qgis/edition/ 
+chown -R :www-data /var/www/lizmap-web-client-2.10.0/temp/ \
+    /var/www/lizmap-web-client-2.10.0/lizmap/var/ \
+    /var/www/lizmap-web-client-2.10.0/lizmap/www \
+    /var/www/lizmap-web-client-2.10.0/lizmap/install/qgis/edition/ 
 
-chmod -R 775 /var/www/lizmap-web-client-master/temp/ \
-    /var/www/lizmap-web-client-master/lizmap/var/ \
-    /var/www/lizmap-web-client-master/lizmap/www \
-    /var/www/lizmap-web-client-master/lizmap/install/qgis/edition/
+chmod -R 775 /var/www/lizmap-web-client-2.10.0/temp/ \
+    /var/www/lizmap-web-client-2.10.0/lizmap/var/ \
+    /var/www/lizmap-web-client-2.10.0/lizmap/www \
+    /var/www/lizmap-web-client-2.10.0/lizmap/install/qgis/edition/
 
-rm -rf /var/www/lizmap-web-client-master/temp/lizmap/*
+rm -rf /var/www/lizmap-web-client-2.10.0/temp/lizmap/*
 
 #dupliquer lizmap en plusieurs sites
-cp -a /var/www/lizmap-web-client-master /var/www/websig
+cp -a /var/www/lizmap-web-client-2.10.0 /var/www/websig
 rm /var/www/websig/lizmap/var/jauth.db /var/www/websig/lizmap/var/logs.db /var/www/websig/lizmap/var/config/lizmapConfig.ini.php
 mkdir /home2  
 touch /home2/jauth.db /home2/logs.db /home2/lizmapConfig.ini.php
@@ -28,4 +28,3 @@ rm -R /home2/jauth.db /home2/logs.db /home2/lizmapConfig.ini.php
 #attribut les droit
 chown :www-data /var/www/websig/temp/ /var/www/websig/lizmap/var/ /var/www/websig/lizmap/www /var/www/websig/lizmap/install/qgis/edition/ -R
 chmod 775 /var/www/websig/temp/ /var/www/websig/lizmap/var/ /var/www/websig/lizmap/www /var/www/websig/lizmap/install/qgis/edition/ -R
-

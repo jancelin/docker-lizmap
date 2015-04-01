468bd5c3b11f58ba7638a84adc7e188c339d9c66
ANCELIN Julien <julien.ancelin@stlaurent.lusignan.inra.fr>
2015-04-01 10:33:38 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-unzip /var/www/2.11beta5.zip -d /var/www
+unzip /var/www/2.11.0.zip -d /var/www
 
-rm /var/www/2.11beta5.zip
+rm /var/www/2.11.0.zip
 
-chown -R :www-data /var/www/lizmap-web-client-2.11beta5/temp/ \
-    /var/www/lizmap-web-client-2.11beta5/lizmap/var/ \
-    /var/www/lizmap-web-client-2.11beta5/lizmap/www \
-    /var/www/lizmap-web-client-2.11beta5/lizmap/install/qgis/edition/ 
+chown -R :www-data /var/www/lizmap-web-client-2.11.0/temp/ \
+    /var/www/lizmap-web-client-2.11.0/lizmap/var/ \
+    /var/www/lizmap-web-client-2.11.0/lizmap/www \
+    /var/www/lizmap-web-client-2.11.0/lizmap/install/qgis/edition/ 
 
-chmod -R 775 /var/www/lizmap-web-client-2.11beta5/temp/ \
-    /var/www/lizmap-web-client-2.11beta5/lizmap/var/ \
-    /var/www/lizmap-web-client-2.11beta5/lizmap/www \
-    /var/www/lizmap-web-client-2.11beta5/lizmap/install/qgis/edition/
+chmod -R 775 /var/www/lizmap-web-client-2.11.0/temp/ \
+    /var/www/lizmap-web-client-2.11.0/lizmap/var/ \
+    /var/www/lizmap-web-client-2.11.0/lizmap/www \
+    /var/www/lizmap-web-client-2.11.0/lizmap/install/qgis/edition/
 
-rm -rf /var/www/lizmap-web-client-2.11beta5/temp/lizmap/*
+rm -rf /var/www/lizmap-web-client-2.11.0/temp/lizmap/*
 
 #dupliquer lizmap en plusieurs sites
-cp -a /var/www/lizmap-web-client-2.11beta5 /var/www/websig
+cp -a /var/www/lizmap-web-client-2.11.0 /var/www/websig
 rm /var/www/websig/lizmap/var/jauth.db /var/www/websig/lizmap/var/logs.db /var/www/websig/lizmap/var/config/lizmapConfig.ini.php
 mkdir /home2  
 touch /home2/jauth.db /home2/logs.db /home2/lizmapConfig.ini.php

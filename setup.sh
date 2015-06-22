de13a7c57c5e275982215c58da619198386df50a
ANCELIN Julien <julien.ancelin@stlaurent.lusignan.inra.fr>
2015-06-22 17:11:41 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,7 +17,7 @@ cd ../../..
  php /var/www/websig/lizmap/install/installer.php
 
 mkdir /home2  
-
+rm /var/www/websig/lizmap/var/db/jauth.db /var/www/websig/lizmap/var/db/logs.db /var/www/websig/lizmap/var/config/lizmapConfig.ini.php /var/www/websig/lizmap/var/config/installer.ini.php /var/www/websig/lizmap/var/config/profiles.ini.php /var/www/websig/lizmap/var/config/localconfig.ini.php
 touch /home2/jauth.db /home2/logs.db /home2/lizmapConfig.ini.php /home2/installer.ini.php /home2/profiles.ini.php /home2/localconfig.ini.php
 ln -s /home2/jauth.db /var/www/websig/lizmap/var/db/jauth.db
 ln -s /home2/logs.db /var/www/websig/lizmap/var/db/logs.db

6e000661f536d094b3347e27b1bfb251d48d69c1
ANCELIN Julien <julien.ancelin@stlaurent.lusignan.inra.fr>
2015-06-22 13:23:18 +0200

    Update setup3.sh

--- setup3.sh
+++ setup3.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-unzip /var/www/master.zip -d /var/www/websig
-
+unzip /var/www/master.zip -d /var/www/
+mv /var/www/master/ /var/www/websig
 rm /var/www/websig/master.zip
 
+cd /var/www/websig/
+lizmap/install/set_rights.sh www-data www-data
 
-
+cd lizmap/var/config
+cp lizmapConfig.ini.php.dist lizmapConfig.ini.php
+cp localconfig.ini.php.dist localconfig.ini.php
+cp profiles.ini.php.dist profiles.ini.php
+cd ../../..

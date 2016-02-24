70de2c5e9bef4d7ba110b58874801e62d145e77c
ANCELIN Julien <julien.ancelin@stlaurent.lusignan.inra.fr>
2016-02-24 14:23:13 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
 
+# unzip lizmap master
+unzip /var/www/master.zip -d /var/www/
+mv /var/www/lizmap-web-client-master/ /var/www/websig/
+rm /var/www/master.zip
+# Set rights & active config
+chmod +x /var/www/websig/lizmap/install/set_rights.sh
+/var/www/websig/lizmap/install/set_rights.sh www-data www-data
+cp /var/www/websig/lizmap/var/config/lizmapConfig.ini.php.dist /var/www/websig/lizmap/var/config/lizmapConfig.ini.php
+cp /var/www/websig/lizmap/var/config/localconfig.ini.php.dist /var/www/websig/lizmap/var/config/localconfig.ini.php
+cp /var/www/websig/lizmap/var/config/profiles.ini.php.dist /var/www/websig/lizmap/var/config/profiles.ini.php
+#  Installer
+php /var/www/websig/lizmap/install/installer.php
+# Set rights
+chown :www-data  /var/www/websig/lizmap/www -R
+chmod 775  /var/www/websig/lizmap/www -R
+chown :www-data /var/www/websig/lizmap/var -R
+chmod 775  /var/www/websig/lizmap/var -R

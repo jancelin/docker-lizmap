179d01f72cdc662dc572f85a2ce66216ca858812
ANCELIN Julien <julien.ancelin@inra.fr>
2017-02-20 09:10:12 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-docker run --restart="always" --name="qgis-lizmap"  --hostname="qgis-lizmap" -p 8081:80  -d -t jancelin/docker-lizmap
+docker run --restart="always" --name="qgis-lizmap"  --hostname="qgis-lizmap" -p 8081:80  -d -t jancelin/docker-lizmap:3.1-2.14LTR
 echo "Now point your browser at: http://localhost:8081/websig/lizmap/www/index.php"

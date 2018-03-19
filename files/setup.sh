fe070ec21aa1e724310fad901d03e0bb6b4a26eb
ANCELIN Julien <julien.ancelin@inra.fr>
2018-03-19 14:18:55 +0100

    personal lizmapConfig.ini.php localconfig.ini.php

--- files/setup.sh
+++ files/setup.sh
@@ -23,8 +23,8 @@ rm /var/www/$LIZMAPVERSION.zip
 # Set rights & active config
 chmod +x /var/www/websig/lizmap/install/set_rights.sh
 /var/www/websig/lizmap/install/set_rights.sh www-data www-data
-cp /var/www/websig/lizmap/var/config/lizmapConfig.ini.php.dist /var/www/websig/lizmap/var/config/lizmapConfig.ini.php
-cp /var/www/websig/lizmap/var/config/localconfig.ini.php.dist /var/www/websig/lizmap/var/config/localconfig.ini.php
+cp /home/files/lizmapConfig.ini.php /var/www/websig/lizmap/var/config/lizmapConfig.ini.php
+cp /home/files/localconfig.ini.php /var/www/websig/lizmap/var/config/localconfig.ini.php
 cp /var/www/websig/lizmap/var/config/profiles.ini.php.dist /var/www/websig/lizmap/var/config/profiles.ini.php
 #  Installer
 php /var/www/websig/lizmap/install/installer.php
@@ -34,4 +34,4 @@ chmod 775  /var/www/websig/lizmap/www -R
 chown :www-data /var/www/websig/lizmap/var -R
 chmod 775  /var/www/websig/lizmap/var -R
 cp -avr /var/www/websig/lizmap/var var/www/websig/lizmap/var_install
-
+/var/www/websig/lizmap/install/set_rights.sh www-data www-data

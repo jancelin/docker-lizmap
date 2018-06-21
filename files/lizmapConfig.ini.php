73bdd3020c7c69a9015da8d0457c4186d43fb883
ANCELIN Julien <julien.ancelin@inra.fr>
2018-06-21 10:06:48 +0200

    Update lizmapConfig.ini.php

--- files/lizmapConfig.ini.php
+++ files/lizmapConfig.ini.php
@@ -1,29 +1,6 @@
 ;<?php die(''); ?>
 ;for security reasons , don't remove or modify the first line
 
-; put here configuration variables that are specific to this installation
-
-
-; chmod for files created by Lizmap and Jelix
-;chmodFile=0664
-;chmodDir=0775
-
-
-
-[modules]
-;; uncomment it if you want to use ldap for authentication
-;; see documentation to complete the ldap configuration
-;ldapdao.access=1
-
-
-[coordplugin_auth]
-;; uncomment it if you want to use ldap for authentication
-;; see documentation to complete the ldap configuration
-;driver=ldapdao
-root@77ffb857c404:/var/www/websig/lizmap/var# cat ./config/lizmapConfig.ini.php
-;<?php die(''); ?>
-;for security reasons , don't remove or modify the first line
-
 ;Services
 ;list the different map services (servers, generic parameters, etc.)
 [services]

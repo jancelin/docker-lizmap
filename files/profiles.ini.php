c00cf266d030bc59dd02d654bc6fa2fd46ea2ee8
gerald2545 <gerald.salin@gmail.com>
2018-11-05 06:04:29 +0100

    add ressources to allow lizmap to store auth data + logs in postgresql database

---
+++ files/profiles.ini.php
@@ -0,0 +1,100 @@
+;<?php die(''); ?>
+;for security reasons, don't remove or modify the first line
+
+[jdb]
+
+; name of the default profile to use for any connection
+default=jauth
+jacl2_profile=jauth
+
+;[jdb:jauth]
+;driver=sqlite3
+;database="var:db/jauth.db"
+
+;[jdb:lizlog]
+;driver=sqlite3
+;database="var:db/logs.db"
+[jdb:jauth]
+driver="pgsql"
+database="###DB_AUTH_NAME###"
+host="###DB_HOST###"
+user="###DB_USER###"
+password="###DB_PASSWORD###"
+
+[jdb:lizlog]
+driver="pgsql"
+database="###DB_LOGS_NAME###"
+host="###DB_HOST###"
+user="###DB_USER###"
+password="###DB_PASSWORD###"
+; when you have charset issues, enable force_encoding so the connection will be
+; made with the charset indicated in jelix config
+;force_encoding = on
+
+; with the following parameter, you can specify a table prefix which will be
+; applied to DAOs automatically. For manual jDb requests, please use method
+; jDbConnection::prefixTable().
+;table_prefix =
+
+; Example for pdo :
+;driver=pdo
+;dsn=mysql:host=localhost;dbname=test
+;user=
+;password=
+
+
+[jcache]
+
+; name of the default profil to use for cache
+default=myapp
+
+
+[jcache:myapp]
+; disable or enable cache for this profile
+enabled=1
+; driver type (file, db, memcached)
+driver=file
+; TTL used (0 means no expire)
+ttl=0
+
+
+; Automatic cleaning configuration (not necessary with memcached)
+;   0 means disabled
+;   1 means systematic cache cleaning of expired data (at each set or add call)
+;   greater values mean less frequent cleaning
+;automatic_cleaning_factor = 0
+
+; Parameters for file driver :
+
+; directory where to put the cache files (optional default 'JELIX_APP_TEMP_PATH/cache/')
+cache_dir=
+; enable / disable locking file
+file_locking=1
+; directory level. Set the directory structure level. 0 means "no directory structure", 1 means "one level of directory", 2 means "two levels"...
+directory_level=0
+; umask for directory structure (default jelix one : 0775)
+directory_umask=
+; prefix for cache files (default 'jelix_cache')
+file_name_prefix=
+; umask for cache files (default jelix one: 0664)
+cache_file_umask=
+
+; Parameters for db driver :
+
+; dao used (default 'jelix~jcache')
+;dao = ""
+; dbprofil (optional)
+;dbprofile = "" 
+
+
+; Parameters for memcached driver :
+
+; Memcached servers. 
+; Can be a list e.g
+;servers = memcache_host1:11211,memcache_host2:11211,memcache_host3:11211 i.e HOST_NAME:PORT
+;servers = 
+
+[jcache:qgisprojects]
+enabled=1
+driver=file
+ttl=0
--- files/start.sh
+++ files/start.sh
@@ -8,7 +8,8 @@ if [ ! -d $VAR ]; then
   echo "Creating Config file in /var"
   cp -avr /var/www/websig/lizmap/var_install/*  /var/www/websig/lizmap/var
 fi
-
+#replace postgresql variables in profiles.ini.php for log and auth databases
+sed -e 's/###DB_HOST###/${POSTGRES_HOST}/; s/###DB_AUTH_NAME###/${POSTGRES_DB_AUTH_NAME}/; s/###DB_LOGS_NAME###/${POSTGRES_DB_LOGS_NAME}/; s/###DB_USER###/${POSTGRES_USER}/; s/###DB_PASSWORD###/${POSTGRES_PASS}/' /var/www/websig/lizmap/var/config/profiles.ini.php
 #set-rights
   /var/www/websig/lizmap/install/set_rights.sh www-data www-data
 

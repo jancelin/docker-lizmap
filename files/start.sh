c15407c7c98233cbcd74929f2366a9f7f729f414
gerald2545 <gerald.salin@gmail.com>
2018-11-05 07:09:29 +0100

    condition for storing logs and auth in postgresql

--- files/start.sh
+++ files/start.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 #generate config file
 
@@ -8,8 +8,13 @@ if [ ! -d $VAR ]; then
   echo "Creating Config file in /var"
   cp -avr /var/www/websig/lizmap/var_install/*  /var/www/websig/lizmap/var
 fi
+if [ ! -z "$POSTGRES_HOST" ]; then
 #replace postgresql variables in profiles.ini.php for log and auth databases
 sed -i 's/###DB_HOST###/${POSTGRES_HOST}/; s/###DB_AUTH_NAME###/${POSTGRES_DB_AUTH_NAME}/; s/###DB_LOGS_NAME###/${POSTGRES_DB_LOGS_NAME}/; s/###DB_USER###/${POSTGRES_USER}/; s/###DB_PASSWORD###/${POSTGRES_PASS}/' /var/www/websig/lizmap/var/config/profiles.ini.php
+else
+cp /var/www/websig/lizmap/var/config/profiles.ini.php.dist /var/www/websig/lizmap/var/config/profiles.ini.php
+fi
+
 #set-rights
   /var/www/websig/lizmap/install/set_rights.sh www-data www-data
 

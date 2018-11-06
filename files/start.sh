6ffbb530d5cae165423861951461af9171d2275a
gsalin <gerald.salin@inra.fr>
2018-11-06 06:44:29 +0100

    doc in the .env file to help people configuring its installation

--- files/start.sh
+++ files/start.sh
@@ -8,16 +8,22 @@ if [ ! -d $VAR ]; then
   echo "Creating Config file in /var"
   cp -avr /var/www/websig/lizmap/var_install/*  /var/www/websig/lizmap/var
 fi
-if [ ! -z "$POSTGRES_HOST" ]; then
+if [ ! -z "$POSTGRES_DB_AUTH_NAME" ]; then
   #replace postgresql variables in profiles.ini.php for log and auth databases
-  cp /home/files/profiles.ini.php /var/www/websig/lizmap/var/config/profiles.ini.php
-  sed -i "s/###DB_HOST###/${POSTGRES_HOST}/; s/###DB_AUTH_NAME###/${POSTGRES_DB_AUTH_NAME}/; s/###DB_LOGS_NAME###/${POSTGRES_DB_LOGS_NAME}/; s/###DB_USER###/${POSTGRES_USER}/; s/###DB_PASSWORD###/${POSTGRES_PASS}/" /var/www/websig/lizmap/var/config/profiles.ini.php
+  cp /home/files/profiles.ini.php /var/www/websig/lizmap/var/config/profiles.ini.php.dist
+  sed -i "s/###DB_HOST###/${POSTGRES_HOST}/; s/###DB_AUTH_NAME###/${POSTGRES_DB_AUTH_NAME}/; s/###DB_LOGS_NAME###/${POSTGRES_DB_LOGS_NAME}/; s/###DB_USER###/${POSTGRES_USER}/; s/###DB_PASSWORD###/${POSTGRES_PASS}/" /var/www/websig/lizmap/var/config/profiles.ini.php.dist
+  /var/www/websig/lizmap/install/reset.sh install
 else
-  cp /var/www/websig/lizmap/var/config/profiles.ini.php.dist /var/www/websig/lizmap/var/config/profiles.ini.php
+  cp /home/files/profiles.ini.php.dist /var/www/websig/lizmap/var/config/profiles.ini.php
 fi
 
 #set-rights
-  /var/www/websig/lizmap/install/set_rights.sh www-data www-data
+chown :www-data  /var/www/websig/lizmap/www -R
+chmod 775  /var/www/websig/lizmap/www -R
+chown :www-data /var/www/websig/lizmap/var -R
+chmod 775  /var/www/websig/lizmap/var -R
+/var/www/websig/lizmap/install/set_rights.sh www-data www-data
+cp -avr /var/www/websig/lizmap/var var/www/websig/lizmap/var_install
 
 # Apache gets grumpy about PID files pre-existing
 rm -f /var/run/apache2/apache2.pid
